328405adbbc381d49ec3076719789fc92c131700
kovner chalumo <kchalumo@gmail.com>
2018-11-26 21:19:54 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,7 +52,7 @@ def invalid_command
   puts "Please enter a valid command"
   prompt_user
 end
-
+h
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
