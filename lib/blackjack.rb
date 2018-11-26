e79fa29fcaf08c7e4d7f792c0750e563a5f456f7
kovner chalumo <kchalumo@gmail.com>
2018-11-26 20:25:07 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -18,12 +18,14 @@ def get_user_input
   gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts  "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  firstCard = deal_card
+  secCard = deal_card
+  display_card_total(first )
 end
 
 def hit?
